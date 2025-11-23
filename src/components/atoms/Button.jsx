bf498827b0b60ab83797f0729b19dc1b581580ed
Tahsin Haider <tahsin3194@gmail.com>
2025-11-24 02:38:29 +0500

    Feat: add hero section with background, heading, and CTA button

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ children, className = '', rounded = false, ...props }) {
   return (
     <button
-      className={`cursor-pointer px-10 min-h-[68px] ${rounded ? 'rounded-full' : 'rounded-[3px]'} text-[16px] font-bold ${className}`}
+      className={`cursor-pointer px-12 min-h-[68px] ${rounded ? 'rounded-full' : 'rounded-[3px]'} text-[16px] font-bold bg-(--color-bg-primary) max-w-fit ${className}`}
       {...props}
     >
       {children}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import HomeTemplate from '../templates/HomeTemplate';
+
 export default function HomePage() {
-  return <h1 className="text-3xl font-bold text-white">Home Page</h1>;
+  return <HomeTemplate />;
 }
