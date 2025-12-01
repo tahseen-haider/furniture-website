9089e6c4932093862eac59057d747e815bfbfaad
Tahsin Haider <tahsin3194@gmail.com>
2025-12-01 15:36:07 +0500

    Update: add vitest config for ui test with a unit test - npm run test runs all .test.js files - Badge component test to test setup

---
+++ src/components/molecules/Badge/Badge.test.jsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import Badge from './Badge';
+
+describe('Badge', () => {
+  it('renders icon, title, and description correctly', () => {
+    render(<Badge icon="/test-icon.svg" title="My Title" description="Some description" />);
+
+    expect(screen.getByText('My Title')).toBeInTheDocument();
+
+    expect(screen.getByText('Some description')).toBeInTheDocument();
+
+    const icon = screen.getByAltText('My Title');
+    expect(icon).toBeInTheDocument();
+    expect(icon).toHaveAttribute('src', '/test-icon.svg');
+  });
+});
