a8af56d905223d6e91b2c6a592623079447bf547
Tahsin Haider <tahsin3194@gmail.com>
2025-12-02 17:16:55 +0500

    Update: fix scrollToTop issue on routing,

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { MainLayout } from '@templates';
 import { HomePage, ProductsPage, CollectionPage } from '@pages';
+import { ScrollToTop } from '@components';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/collections" element={<CollectionPage />} />
-          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/collections/:product" element={<ProductsPage />} />
+          <Route path="/products" element={<CollectionPage />} />
+          <Route path="/products/:product" element={<ProductsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
