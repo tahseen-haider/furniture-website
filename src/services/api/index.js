b38129379242d01415cd979253b32862ec548eb1
Tahsin Haider <tahsin3194@gmail.com>
2025-12-02 17:16:55 +0500

    Update: fix scrollToTop issue on routing,

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -2,10 +2,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL || '/mock';
 
 export async function http(method, endpoint, body) {
   try {
-    const url =
-      BASE_URL === '/mock'
-        ? `${BASE_URL}/${endpoint}.json` // load mock JSON file
-        : `${BASE_URL}/${endpoint}`; // real backend
+    const url = BASE_URL === '/mock' ? `${BASE_URL}/${endpoint}.json` : `${BASE_URL}/${endpoint}`;
 
     const options = {
       method,
