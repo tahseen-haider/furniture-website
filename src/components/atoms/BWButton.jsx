60babd6d3a5317e781bab8af625ed90420339e48
Tahsin Haider <tahsin3194@gmail.com>
2025-12-04 21:35:22 +0500

    Update: add sidebar filter with debounce - add price and availability filter - add Black&White Button component - add Divider component - add utils for utility methods

---
+++ src/components/atoms/BWButton.jsx
@@ -0,0 +1,19 @@
+const BWButton = ({ text = 'Button', className = '', onClick = () => {}, variant = 'black' }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`
+        ${className} ${
+          variant === 'black'
+            ? 'bg-(--color-text-dark) hover:bg-(--color-text-inverse) text-(--color-text-inverse) hover:text-(--color-text-dark) border border-(--color-text-dark)'
+            : 'bg-(--color-text-inverse) hover:bg-(--color-text-dark) text-(--color-text-dark) hover:text-(--color-text-inverse) border border-(--color-text-dark)'
+        }
+        w-full cursor-pointer h-11 font-normal text-[12px] transition-colors duration-300
+      `}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default BWButton;
--- src/components/organisms/ProductsFilterSidebar.jsx
+++ src/components/organisms/ProductsFilterSidebar.jsx
@@ -1,15 +1,28 @@
 import { X } from 'lucide-react';
-import { Paragraph, Button } from '@components';
+import { Paragraph, BWButton, Divider } from '@components';
 
-const ProductsFilterSidebar = ({ onClose, children }) => {
+const ProductsFilterSidebar = ({ onClose, onClearParams, children }) => {
   return (
-    <div className="p-5 flex flex-col gap-4 h-full">
+    <div className="p-5 flex flex-col gap-4 h-full bg-(--color-surface-300)">
       <div className="flex justify-end">
         <X className="cursor-pointer text-gray-600" onClick={onClose} />
       </div>
       <Paragraph variant="G">Filter By</Paragraph>
-      <div className="flex-1 overflow-y-auto w-full px-2 overflow-x-hidden">{children}</div>
-      <div className="flex flex-col"></div>
+      <Divider />
+      <div className="flex-1 overflow-y-auto w-full px-2 overflow-x-hidden flex flex-col gap-4">
+        {children}
+      </div>
+      <div className="flex flex-col gap-3">
+        <BWButton
+          variant="white"
+          text="CLEAR ALL"
+          onClick={() => {
+            onClearParams();
+            onClose();
+          }}
+        />
+        <BWButton variant="black" text="APPLY FILTERS" onClick={onClose} />
+      </div>
     </div>
   );
 };
