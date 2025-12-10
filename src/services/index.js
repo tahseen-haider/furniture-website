f43e36e171438983055bd7e0dd0a009d800cbb9c
Tahsin Haider <tahsin3194@gmail.com>
2025-12-10 16:42:39 +0500

    Update: add 10 mock products and delay on product fetch

--- src/services/index.js
+++ src/services/index.js
@@ -1,2 +1,9 @@
 export * from './api/products';
 export * from './api/collections';
+
+export const simulateDelay = (promise, delay = 1000) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      promise.then(resolve).catch(reject);
+    }, delay);
+  });
