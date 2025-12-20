b1d9247c2d1f2e8861047551d7c89cc855e8674b
Tahsin Haider <tahsin3194@gmail.com>
2025-12-21 01:33:46 +0500

    Update: add cart to sync with database

--- src/services/index.js
+++ src/services/index.js
@@ -2,6 +2,7 @@ export * from './api/products';
 export * from './api/collections';
 export * from './api/orders';
 export * from './api/auth';
+export * from './api/cart';
 
 export const simulateDelay = (promise, delay = 500) =>
   new Promise((resolve, reject) => {
