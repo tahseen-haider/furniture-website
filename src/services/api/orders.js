a32b47af032c56ff5cc8a3eee0711c5fb4346ef7
Tahsin Haider <tahsin3194@gmail.com>
2025-12-22 18:46:03 +0500

    Update: add order submit and tracking order by id dynamically

--- src/services/api/orders.js
+++ src/services/api/orders.js
@@ -1,8 +1,12 @@
-import { GET } from '@services/api';
-const BASE_URL = '/mock';
+import { GET, POST } from '@services/api';
+
+const BASE_URL = `${import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000'}/api/orders`;
 
 export const ordersAPI = {
+  placeOrder: (orderData) => {
+    return POST(`${BASE_URL}/place-order`, orderData);
+  },
   trackOrderById: (id) => {
-    return GET(`${BASE_URL}/trackOrder.json`);
+    return GET(`${BASE_URL}/track-order/${id}`);
   },
 };
