5049ce5cc54959fb13f22b3012f06c43d6eea8d7
Tahsin Haider <tahsin3194@gmail.com>
2025-12-23 17:19:39 +0500

    Update: add documentation, add dynamic api endpoints - no hardcoded data on frontend now

--- src/services/api/collections.js
+++ src/services/api/collections.js
@@ -1,6 +1,5 @@
 import { GET } from '@services/api';
-const BASE_URL = '/mock';
-
+const BASE_URL = `${import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000'}/api/categories`;
 export const collectionsAPI = {
-  fetchAll: () => GET(`${BASE_URL}/collections.json`),
+  fetchAll: () => GET(`${BASE_URL}/`),
 };
